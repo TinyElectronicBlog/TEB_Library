32feda0e28dffaee25bf83663ef8ba3a89cc4534
Tiny Electronic Blog <89404302+TinyElectronicBlog@users.noreply.github.com>
2024-01-18 21:55:06 +0100

    TEB_TimeManagement.h

--- src/TEB_Library/Time/TEB_TimeManagement.h
+++ src/TEB_Library/Time/TEB_TimeManagement.h
@@ -33,7 +33,7 @@
 */
 
 /**
-       \brief A TEB_TimeManagement_struct can contain a date and a time.
+    \brief A TEB_TimeManagement_struct can contain a date and a time.
 */
 struct TEB_TimeManagement_struct {
   TEB_small_u_int second;
@@ -46,63 +46,63 @@ struct TEB_TimeManagement_struct {
 
 /**
        \brief Determine the first weekday of a year.
-	   \param [in] year Year to consider.
-	   \return The weekday (Sunday = 1, Thursday = 5, ..., Saturday = 7)
+       \param [in] year Year to consider.
+       \return The weekday (Sunday = 1, Thursday = 5, ..., Saturday = 7)
 */
 TEB_small_int TEB_TimeManagement_first_weekday_year (TEB_medium_int year);
 
 /**
        \brief Determines whether a year is a leap year.
-	   \param [in] year Year to consider.
-	   \return 1 if year is a leap year, otherwise 0.
+       \param [in] year Year to consider.
+       \return 1 if year is a leap year, otherwise 0.
 */
 TEB_bool TEB_TimeManagement_is_leap_year (TEB_medium_int year);
 
 /**
        \brief Determines the number of days in a month.
-	   \param [in] is_a_leap_year Put 1 if the month is in a leap year.
-	   \return The number of days in the month.
+       \param [in] is_a_leap_year Put 1 if the month is in a leap year.
+       \return The number of days in the month.
 */
 TEB_small_u_int TEB_TimeManagement_days_of_month (TEB_small_u_int month,
                                                  TEB_bool is_a_leap_year);
 
 /**
-       \brief Converts a struct TEB_TimeManagement_struct to epoch expressed in seconds.
-	   \param [in] time_to_convert Time to convert.
-	   \param [in] initial_year_of_the_epoch Initial year to be associated with epoch = 0.
-	   \return The converted epoch.
+   \brief Converts a struct TEB_TimeManagement_struct to epoch expressed in seconds.
+   \param [in] time_to_convert Time to convert.
+   \param [in] initial_year_of_the_epoch Initial year to be associated with epoch = 0.
+   \return The converted epoch.
 */
 TEB_time_t TEB_TimeManagement_time_to_epoch (const struct TEB_TimeManagement_struct* time_to_convert,
                                             TEB_medium_int initial_year_of_the_epoch);
 
 /**
-       \brief Converts an epoch expressed in seconds to a struct TEB_TimeManagement_struct.
-	   \param [in] epoch_to_convert Epoch to convert.
-	   \param [in] initial_year_of_the_epoch Initial year associated with epoch_to_convert = 0.
-	   \param [in] time_converted Where to save the struct TEB_TimeManagement_struct converted.
-	   \return 1 if all ok.
+        \brief Converts an epoch expressed in seconds to a struct TEB_TimeManagement_struct.
+	\param [in] epoch_to_convert Epoch to convert.
+	\param [in] initial_year_of_the_epoch Initial year associated with epoch_to_convert = 0.
+	\param [in] time_converted Where to save the struct TEB_TimeManagement_struct converted.
+	\return 1 if all ok.
 */
 TEB_small_int TEB_TimeManagement_epoch_to_time (TEB_time_t epoch_to_convert,
                                                TEB_medium_int initial_year_of_the_epoch,
                                                struct TEB_TimeManagement_struct* time_converted);
 
 /**
-       \brief Determines the weekday of an epoch.
-	   \param [in] epoch Epoch to consider.
-	   \param [in] initial_year_of_the_epoch Year associated with epoch = 0.
-	   \param [in] initial_weekday_of_the_epoch Weekday associated with epoch = 0. 
-	   \return The weekday determined.
+        \brief Determines the weekday of an epoch.
+	\param [in] epoch Epoch to consider.
+	\param [in] initial_year_of_the_epoch Year associated with epoch = 0.
+	\param [in] initial_weekday_of_the_epoch Weekday associated with epoch = 0. 
+	\return The weekday determined.
 */
 TEB_small_int TEB_TimeManagement_weekday_from_epoch (TEB_time_t epoch,
                                                     TEB_medium_int initial_year_of_the_epoch,
                                                     TEB_small_int initial_weekday_of_the_epoch);
 
 /**
-       \brief Determines the weekday of a date.
-	   \param [in] year Year to consider.
-	   \param [in] month Month to consider.
-	   \param [in] day Day to consider.
-	   \return The weekday determined.
+        \brief Determines the weekday of a date.
+	\param [in] year Year to consider.
+	\param [in] month Month to consider.
+	\param [in] day Day to consider.
+	\return The weekday determined.
 */
 TEB_small_int TEB_TimeManagement_weekday_from_date (TEB_medium_int year,
                                                    TEB_small_u_int month,
@@ -112,7 +112,7 @@ TEB_small_int TEB_TimeManagement_weekday_from_date (TEB_medium_int year,
        \brief Evaluates whether one time instant is prior to another.
        \param [in] time_to_evaluate Time instant to evaluate.
        \param [in] reference_time Reference time instant.
-	   \return 1 if time_to_evaluate < reference_time.
+       \return 1 if time_to_evaluate < reference_time.
     */
 TEB_bool TEB_TimeManagement_is_time_passed (const struct TEB_TimeManagement_struct* time_to_evaluate,
 								            const struct TEB_TimeManagement_struct* reference_time);
@@ -121,9 +121,9 @@ TEB_bool TEB_TimeManagement_is_time_passed (const struct TEB_TimeManagement_stru
        \brief This function evaluates whether a time is daylight saving or solar time.
        \param [in] time_to_evaluate Time to evaluate.
        \param [in] dst_start When DST starts (month, weekday, th_weekday, hour, minute). For example, the second weekday is denoted by dst_start[2]=2, the penultimate weekday by dst_start[2]=-2.
-	   \param [in] dst_end When DST ends (month, weekday, th_weekday, hour, minute). For example, the first weekday is denoted by dst_end[2]=1, the third-to-last weekday by dst_end[2]=-3.
-	   \return 1 if time is daylight saving time, 0 if not, -1 if the evaluation is not possible.
-	   \details time, dst_start and dst_end must refer to the same time zone. Set dst_start[0] = dst_end[0] = 0 to consider the time to be always solar time. Set dst_start[0] = dst_end[0] = -1 to consider the time to be always daylight saving time.
+       \param [in] dst_end When DST ends (month, weekday, th_weekday, hour, minute). For example, the first weekday is denoted by dst_end[2]=1, the third-to-last weekday by dst_end[2]=-3.
+       \return 1 if time is daylight saving time, 0 if not, -1 if the evaluation is not possible.
+       \details time, dst_start and dst_end must refer to the same time zone. Set dst_start[0] = dst_end[0] = 0 to consider the time to be always solar time. Set dst_start[0] = dst_end[0] = -1 to consider the time to be always daylight saving time.
     */
 TEB_small_int TEB_TimeManagement_is_dst_time (const struct TEB_TimeManagement_struct* time_to_evaluate,
                                              const TEB_small_int dst_start[5],
@@ -133,10 +133,10 @@ TEB_small_int TEB_TimeManagement_is_dst_time (const struct TEB_TimeManagement_st
        \brief Evaluates whether an epoch is daylight saving time or standard time.
        \param [in] epoch_to_evaluate Epoch to evaluate.
        \param [in] dst_start_epoch Epoch of the start of dst to be considered. Is a solar time by definition.
-	   \param [in] dst_end_epoch Epoch of the end of dst to be considered. Is a daylight saving time by definition.
-	   \param [in] dst_end_epoch_is_standard_time Set to 0 if dst_end_epoch is provided as daylight saving time, 1 if solar.
-	   \return 1 if epoch_to_evaluate is daylight saving time, 0 otherwise.
-	   \details The input parameters must refer to the same time zone.
+       \param [in] dst_end_epoch Epoch of the end of dst to be considered. Is a daylight saving time by definition.
+       \param [in] dst_end_epoch_is_standard_time Set to 0 if dst_end_epoch is provided as daylight saving time, 1 if solar.
+       \return 1 if epoch_to_evaluate is daylight saving time, 0 otherwise.
+       \details The input parameters must refer to the same time zone.
     */
 TEB_bool TEB_TimeManagement_is_dst_epoch (TEB_time_t epoch_to_evaluate,
                                          TEB_time_t dst_start_epoch,
@@ -144,9 +144,9 @@ TEB_bool TEB_TimeManagement_is_dst_epoch (TEB_time_t epoch_to_evaluate,
                                          TEB_bool dst_end_epoch_is_standard_time);
 
 /**
-       \brief Determines the epoch of the beginning or end of a dst.
-       \param [in] year Year to be considered.
-       \param [in] month Month to be considered.
+           \brief Determines the epoch of the beginning or end of a dst.
+           \param [in] year Year to be considered.
+           \param [in] month Month to be considered.
 	   \param [in] weekday The day of the week on which DST begins/ends. Enter 1 for Sunday, 2 for Monday, 3 for Tuesday, ..., 7 for Saturday.
 	   \param [in] th_weekday Enter 1 to select the first dayOfWeek of the month or -1 for the last, 2 for the second dayOfWeek of the month or -2 for the penultimate ... (max +5 or -5 included).
 	   \param [in] hour Hour to be considered.
@@ -168,9 +168,9 @@ TEB_time_t TEB_TimeManagement_get_dst_epoch (TEB_medium_int year,
                                             TEB_bool get_dst_end_in_standard_time);
 
 /**
-       \brief Converts an epoch from utc to local (subject to time zone).
-       \param [in] epoch_to_convert The epoch to convert.
-       \param [in] time_zone_utc_offset_hour Enter an UTC hour offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
+           \brief Converts an epoch from utc to local (subject to time zone).
+           \param [in] epoch_to_convert The epoch to convert.
+           \param [in] time_zone_utc_offset_hour Enter an UTC hour offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
 	   \param [in] time_zone_utc_offset_minute Enter an UTC minute offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
 	   \return The converted epoch.
 	   \details The function does not take into account daylight saving time.
@@ -180,9 +180,9 @@ TEB_time_t TEB_TimeManagement_utc_to_local_epoch (TEB_time_t epoch_to_convert,
                                                  TEB_small_int time_zone_utc_offset_minute);
 
 /**
-       \brief Converts an epoch from local (subject to time zone) to utc.
-       \param [in] epoch_to_convert The epoch to convert.
-       \param [in] time_zone_utc_offset_hour Enter an UTC hour offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
+           \brief Converts an epoch from local (subject to time zone) to utc.
+           \param [in] epoch_to_convert The epoch to convert.
+           \param [in] time_zone_utc_offset_hour Enter an UTC hour offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
 	   \param [in] time_zone_utc_offset_minute Enter an UTC minute offset in standard time (for more information see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
 	   \return The converted epoch.
 	   \details The function does not take into account daylight saving time.
@@ -262,7 +262,7 @@ TEB_small_u_int TEB_TimeManagement_str_int_s (const TEB_char* time);
 /**
        \brief Converts a HTTP-date to epoch.
        \param [in] http_date_to_convert Date and time to convert provided in a similar way to the "HTTP-date" format (for example "15 Nov 1994 08:12:31"). For more information see the definition of the method.
-	   \param [in] initial_year_of_the_epoch Initial year of the converted epoch.
+       \param [in] initial_year_of_the_epoch Initial year of the converted epoch.
        \return The epoch in seconds.
     */
 TEB_time_t TEB_TimeManagement_http_date_to_epoch (const TEB_char* http_date_to_convert,
